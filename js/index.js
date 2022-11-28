d7413de7ef79d61a4b2799eccda6ada4feebbd03
HalDevOne <haldevone@gmail.com>
2022-11-28 10:15:06 +0100

    REDIRECT PORTFOLIO

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const navToggle = document.querySelector(".nav-toggle");
 const navLinks = document.querySelectorAll(".nav__link");
 
 
-window.location.href = "https://haldevone.com/";
+window.location.href = "https://haldevone.com/portfolio/";
 
 navToggle.addEventListener("click", () => {
     document.body.classList.toggle("nav-open");
