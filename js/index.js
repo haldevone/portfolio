47bccc33425715754947082b903685607db6d558
HalDevOne <haldevone@gmail.com>
2022-11-28 10:11:17 +0100

    redirect

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const navToggle = document.querySelector(".nav-toggle");
 const navLinks = document.querySelectorAll(".nav__link");
 
 
-// const cardToogle = document.querySelector(".fig-one");
+window.location.href = "https://haldevone.com/";
 
 navToggle.addEventListener("click", () => {
     document.body.classList.toggle("nav-open");
